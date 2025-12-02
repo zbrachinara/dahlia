ef0ae1164edc76ee85d1eb25acbf8525b32d2ff9
Eric Yu <zbrachinara@gmail.com>
2025-12-02 12:24:26 -0500

    add skeleton for erase function

--- src/main/scala/typechecker/IntegrityCheck.scala
+++ src/main/scala/typechecker/IntegrityCheck.scala
@@ -1,8 +1,15 @@
 package typechecker
 
-import fuselang.common.Syntax.Prog
+import fuselang.common.Syntax.{Decl, Definition, FuncDef, Id, Prog, RecordDef, TSecLabeled, TVoid, Type}
 
 object IntegrityCheck {
+
+  private def walk_definitions(program : Prog) : Iterable[Definition] =
+    val Prog(includes, defs, _, decls, cmd) = program
+    val allDefs = includes.flatMap(_.defs) ++ defs
+    val topFunc = FuncDef(Id(""), decls, TVoid(), Some(cmd))
+    allDefs ++ List(topFunc)
+
   // TODO should this also check or add annotations for timing information?
   /**
    * Checks that a program satisfies its security labels wrt information flow
@@ -11,6 +18,9 @@ object IntegrityCheck {
    */
   def integrityCheck(program : Prog) : Unit = ???
 
+  private def strip_label(ty : Type) : Type = ty match
+    case TSecLabeled(datatype, _, _) => datatype
+    case x => x
   /**
    * Erases security labels from types.
    * All types containing security labels are unwrapped and replaced with the type inside,
@@ -18,5 +28,16 @@ object IntegrityCheck {
    *
    * @param program A valid program, optionally containing security labels
    */
-  def erase(program : Prog) : Unit = ???
+  def erase(program : Prog) : Unit = {
+    for (definition <- walk_definitions(program)) {
+      definition match
+        case FuncDef(_, args, retTy, body) => {
+          for (arg <- args) {
+//            arg.typ = strip_label(arg.typ)
+          }
+          ???
+        }
+        case RecordDef(_, _) => ???
+    }
+  } // TODO crawl all declarations
 }
