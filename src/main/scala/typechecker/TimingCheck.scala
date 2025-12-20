86ac90122a2524b89adc17a1403942f04019f93b
Eric Yu <zbrachinara@gmail.com>
2025-12-19 19:38:35 -0500

    add case for EPhysAccess

--- src/main/scala/typechecker/TimingCheck.scala
+++ src/main/scala/typechecker/TimingCheck.scala
@@ -3,7 +3,7 @@ package typechecker
 import fuselang.common.Checker.{Checker, PartialChecker}
 import fuselang.common.EnvHelpers.{ScopeManager, UnitEnv}
 import fuselang.common.Syntax
-import fuselang.common.Syntax.{CIf, CLet, CPar, CRange, CSeq, Command, Decl, Definition, EApp, EArrAccess, EArrLiteral, EBinop, EBool, ECast, EInt, ERational, ERecAccess, ERecLiteral, EVar, Expr, FuncDef, Id, Prog, RecordDef, SecurityLabel, TSecLabeled, TVoid, Type}
+import fuselang.common.Syntax.{CIf, CLet, CPar, CRange, CSeq, Command, Decl, Definition, EApp, EArrAccess, EArrLiteral, EBinop, EBool, ECast, EInt, EPhysAccess, ERational, ERecAccess, ERecLiteral, EVar, Expr, FuncDef, Id, Prog, RecordDef, SecurityLabel, TSecLabeled, TVoid, Type}
 import fuselang.typechecker.SecurityEnv
 
 object TimingCheck {
@@ -20,17 +20,17 @@ object TimingCheck {
       case EBinop(_, l, r) => level_of_expr(l).max(level_of_expr(r))
       case EArrAccess(x, e) => env.label_of_id(x).max(level_of_expr_seq(e))
       // TODO figure out the deal with EPhysAccess
+      case _ : EPhysAccess => throw new RuntimeException("Internal Error: Should be unreachable at this stage")
       case EArrLiteral(e) => level_of_expr_seq(e)
       case ERecAccess(e, x) => env.label_of_id(x).max(level_of_expr(e)) 
       case ERecLiteral(e) => level_of_expr_seq(e.values.toSeq)
       case EApp(_, e) => level_of_expr_seq(e)
       case EVar(x) => env.label_of_id(x)
       case ECast(e, _) => level_of_expr(e)
-      case _ => ???
 
     override def checkC(cmd : Command)(implicit env : Env): Env = mergeCheckC({
       case (c @ CIf(cond, c1, c2), env) =>
-        val condEnv = checkE(cond)
+        val cond_label = level_of_expr(cond)
         ???
     })(cmd, env)
   }
